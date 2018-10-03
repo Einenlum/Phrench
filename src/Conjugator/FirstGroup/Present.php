c00115d497588c325732899dbe98bc9a22ee72a1
Einenlum <yann.rabiller@gmail.com>
2018-10-03 02:51:04 +0200

    Add dangerous ending consonants

--- src/Conjugator/FirstGroup/Present.php
+++ src/Conjugator/FirstGroup/Present.php
@@ -8,8 +8,16 @@ use Phrench\ConjugatorInterface;
 use Phrench\DTO\ConjugationModality;
 use Phrench\DTO\Verb;
 
+/**
+ * @TODO: appeler -> appelle, acheter -> achète
+ */
 class Present implements ConjugatorInterface
 {
+    const DANGEROUS_ENDING_CONSONANTS = [
+        'c' => 'ç',
+        'g' => 'ge',
+    ];
+
     public function conjugate(Verb $verb, ConjugationModality $modality): string
     {
         switch ($modality->getPerson()) {
@@ -20,7 +28,7 @@ class Present implements ConjugatorInterface
             case ConjugationModality::PERSON_THIRD_SINGULAR:
                 return $this->append($verb, 'e');
             case ConjugationModality::PERSON_FIRST_PLURAL:
-                return $this->append($verb, 'ons');
+                return $this->append($verb, 'ons', true);
             case ConjugationModality::PERSON_SECOND_PLURAL:
                 return $this->append($verb, 'ez');
             case ConjugationModality::PERSON_THIRD_PLURAL:
@@ -33,8 +41,31 @@ class Present implements ConjugatorInterface
         return $modality->getTense() === ConjugationModality::TENSE_PRESENT;
     }
 
-    private function append(Verb $verb, string $suffix): string
+    private function append(Verb $verb, string $suffix, bool $replaceDangerousEnding = false): string
     {
-        return sprintf('%s%s', $verb->getStem(), $suffix);
+        if (!$this->isEndingDangerous($verb->getStem()) || !$replaceDangerousEnding) {
+            return $verb->getStem() . $suffix;
+        }
+
+        $removeLastEvilLetter = function(string $stem) {
+            return substr($stem, 0, strlen($stem) - 1);
+        };
+        $getFunnyReplacement = function(string $stem) {
+            return self::DANGEROUS_ENDING_CONSONANTS[substr($stem, -1)];
+        };
+
+        $newStem = $removeLastEvilLetter($verb->getStem()) . $getFunnyReplacement($verb->getStem());
+
+        return $newStem . $suffix;
+    }
+
+    private function isEndingDangerous(string $stem): bool
+    {
+        $lastLetter = substr($stem, -1);
+
+        return in_array(
+            $lastLetter,
+            array_keys(self::DANGEROUS_ENDING_CONSONANTS)
+        );
     }
 }
